1a4b7231f365caa55c25a744d0d8892bcd40b49f
Ronak-Malkan <ronakmalkan2062001@gmail.com>
2023-09-25 11:55:15 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GatsbyTrial`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
