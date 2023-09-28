e79fd9e442dadc82a97062abfaaf5e2c88b8cca4
Ronak-Malkan <ronakmalkan2062001@gmail.com>
2023-09-28 15:51:42 +0530

    created first practise site using gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,18 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `GatsbyTrial`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+   siteMetadata: {
+      title: `GatsbyTrial`,
+      siteUrl: `https://www.yourdomain.tld`,
+   },
+   plugins: [
+      {
+         resolve: "gatsby-source-filesystem",
+         options: {
+            name: `blog`,
+            path: `${__dirname}/blog`,
+         },
+      },
+      "gatsby-plugin-mdx",
+   ],
+};
